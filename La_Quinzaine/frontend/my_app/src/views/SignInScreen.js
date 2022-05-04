a195067f395a895d8871cc2adf99215f284e3152
henri heunen <heunen.h@gmail.com>
2022-05-04 20:51:06 +0200

    fdqsf

---
+++ La_Quinzaine/frontend/my_app/src/views/SignInScreen.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import {Button,View, Text, EventEmitter } from 'react-native';
+import MyButton from '../components/menu_button';
+import MyTextInput from '../components/text_input';
+
+const SigninScreen = () =>{
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
+    const onSignInPressed = () =>{
+        console.warn('sign in');
+    }
+   return(
+   <View>
+        <Text style={{fontSize:28}}>Utilisateur :</Text>
+
+        <MyTextInput 
+        placeholder='Utilisateur'
+        value={username}
+        setValue={setUsername}
+        ></MyTextInput>
+
+        <Text style={{fontSize:28}}>Mot de Passe :</Text>
+
+        <MyTextInput
+         placeholder='Mot de passe'
+         value={password}
+         setValue={ setPassword}
+         secureTextEntry
+        ></MyTextInput>
+
+        <MyButton onPress={onSignInPressed} text='Sign in'></MyButton>
+        
+    </View>
+)}
+
+export default SigninScreen;
