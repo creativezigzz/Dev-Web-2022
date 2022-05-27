d298488ce1bc19eb2313a3a84577be8fe3f6f0c6
creativezigzz <64020049+creativezigzz@users.noreply.github.com>
2022-05-27 15:46:54 +0200

    v1

--- La_Quinzaine/frontend/my_app/App.js
+++ La_Quinzaine/frontend/my_app/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, View, Text, EventEmitter, StyleSheet, Pressable, ScrollView, ImageBackground } from 'react-native';
+import { Button, View, Text, EventEmitter, StyleSheet, Pressable, ScrollView, ImageBackground,AsyncStorage } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Menu from './src/views/Menu';
