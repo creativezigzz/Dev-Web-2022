59cf2221856daca9d1f6a0caa18899fc07690f6e
henri heunen <heunen.h@gmail.com>
2022-05-30 01:23:46 +0200

    µ

--- La_Quinzaine/frontend/my_app/src/views/Beers.js
+++ La_Quinzaine/frontend/my_app/src/views/Beers.js
@@ -2,23 +2,23 @@ import React, { useState,useEffect } from "react";
 import { Pressable, View, Text, FlatList, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import BarreDeRecherche from "../components/BarreDeRecherche";
 import {postData, getData} from "../context/fetchContext"
+
 const controller = new AbortController();
 const {signal} = controller;
 
 const MyBeers = (props) => {
 
-   
 
     useEffect(() => {
         getData('http://localhost:3000/api/beers/').then(data => setBeerList(data.data));    // add conditional check
     }, []); 
-    
       
 
-    const [lookBeer, setLookBeer] = useState(''); 
-    const [beerList,setBeerList]=useState([])  
-    const goNav = props.goNav;
+    const [lookBeer, setLookBeer] = useState('');
+    const [beerList, setBeerList] = useState()  // ICI METTRE FETCH DE TOUTES LES BIERES PAR ORDRE ALPHABETIQUE MAJEUR ET QUANTITE MINEUR DANS LE USESTATE()
 
+    const goNav = props.goNav;
+    
     const AfficherBeer = () => {
         return (
             <View>
@@ -26,11 +26,11 @@ const MyBeers = (props) => {
                 <FlatList
 
                     data={beerList}
-                   //UTILISE beerList DU USESTATE POUR AVOIR UN TRUC REACTIF
+                    //UTILISE beerList DU USESTATE POUR AVOIR UN TRUC REACTIF
                     keyExtractor={(item) => item.idBeer}
-                    renderItem={({ item }) =>
+                    renderItem={({item}) =>
                         <BeerInfo beerId={item.idBeer} beerName={item.beerName} degree={item.degree}
-                            price={item.price} quantity={item.quantite} source={item.imageUrl}></BeerInfo>
+                                  price={item.price} quantity={item.quantite} source={item.imageUrl}></BeerInfo>
                     }
 
                 />
@@ -41,15 +41,14 @@ const MyBeers = (props) => {
     const AfficherInfoBeer = () => {
 
 
-
         return (
-            <View style={{ padding: 5, flexDirection: 'row', marginBottom: 5 }}>
+            <View style={{padding: 5, flexDirection: 'row', marginBottom: 5}}>
 
-                <Text style={{ flex: 11 }}>Bière :</Text>
-                <Text style={{ flex: 9 }}>Degrée :</Text>
-                <Text style={{ flex: 6 }}>Prix :</Text>
-                <Text style={{ flex: 9 }}>Quantité :</Text>
-                <Text style={{ flex: 7 }}>Image :</Text>
+                <Text style={{flex: 11}}>Bière :</Text>
+                <Text style={{flex: 9}}>Degrée :</Text>
+                <Text style={{flex: 6}}>Prix :</Text>
+                <Text style={{flex: 9}}>Quantité :</Text>
+                <Text style={{flex: 7}}>Image :</Text>
 
             </View>
         )
@@ -72,8 +71,8 @@ const MyBeers = (props) => {
         }
 
         return (
-            <View >
-                <TouchableOpacity style={{ flexDirection: 'row' }} onPress={onClick}>
+            <View>
+                <TouchableOpacity style={{flexDirection: 'row'}} onPress={onClick}>
                     <Text style={style.nameBiereList}>{props.beerName}</Text>
                     <Text style={style.textBiereList}>{props.degree}</Text>
                     <Text style={style.textBiereList}>{props.price} €</Text>
@@ -85,43 +84,42 @@ const MyBeers = (props) => {
     }
 
     return (
-        <View style={{ flex: 1 }}>
-            <View style={{ height: 25, flexDirection: 'row',marginTop:25 }}>
-                <BarreDeRecherche style={{ flex: 3 }} value={lookBeer} setValue={setLookBeer} placeholder={'Recherche de Biere'}></BarreDeRecherche>
-                <Pressable style={{flex:1}}onPress={bouttonRecheche}>
-                    <Image style={{height:25, resizeMode: 'contain',borderColor:'black',borderWidth:1,width:42,marginHorizontal:25 }} source={require('../data/images/search.png')}></Image>
-                </Pressable>
+        <View style={{flex: 1}}>
+            <View style={{height: 25, flexDirection: 'row', marginTop: 25}}>
+                <BarreDeRecherche style={{flex: 3}} value={lookBeer} setValue={setLookBeer}
+                 placeholder={'Recherche de Biere'}></BarreDeRecherche>
             </View>
-            <View style={{ padding: 10, flex: 1 }}>
+            <View style={{padding: 10, flex: 1}}>
                 <AfficherBeer goNav={props.goNav}></AfficherBeer>
             </View>
             <View>
             </View>
 
-        </View >
+        </View>
 
     )
 }
 
 
 const style = StyleSheet.create({
-    imageBiere: {
-        height: 80,
-        flex: 6
-    },
-    textBiereList: {
-        flex: 6,
-        height: 120,
-        width: 80,
-        fontSize: 18,
-    },
-    nameBiereList: {
-        flex: 10,
-        height: 125,
-        width: 802,
-        fontSize: 18,
+        imageBiere: {
+            height: 80,
+            flex: 6
+        },
+        textBiereList: {
+            flex: 6,
+            height: 120,
+            width: 80,
+            fontSize:
+                18,
+        },
+        nameBiereList: {
+            flex: 10,
+            height: 125,
+            width: 802,
+            fontSize: 18,
+        }
     }
-}
 )
 
 export default MyBeers;
