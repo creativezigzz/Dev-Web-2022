7c534e2f593ea7f008a59d3e4a31a10a684cb2fe
henri heunen <heunen.h@gmail.com>
2022-08-29 02:12:38 +0200

    Rajout de la date en fr, et d'un léger style sur la page event, ELEMENT MANQUANT:Tri par date

--- La_Quinzaine/frontend/my_app/src/views/EventPage.js
+++ La_Quinzaine/frontend/my_app/src/views/EventPage.js
@@ -13,32 +13,34 @@ const MyEvents = () => {
     // composant qui crée l'architecture d'un objet event
     const EventInfo = (props) => {
         const [brewery, setBrewery] = useState({
-            idBrewery : 1,
+            idBrewery: 1,
             breweryDescript: 'Loading',
             breweryName: 'Loading',
-            urlImage:'null'
+            urlImage: 'null'
         });
 
-          useEffect(() => {
-        getData('http://localhost:3000/api/brewery/id/' + props.breweryId ).then(d => setBrewery(d.data))// add conditional check
+        useEffect(() => {
+            getData('http://localhost:3000/api/brewery/id/' + props.breweryId).then(d => setBrewery(d.data))// add conditional check
         }, [props.breweryId])
 
-        console.log(brewery)
+        console.log(Date(props.dateString))
 
         //VIEW d'une brasserie
         return (
-            <View>
-                <Text>{props.dateString}</Text>
+            <View style={{marginTop:5,margin:5}}>
+                <Text style={{fontSize:16,marginLeft:5}}>{stringToDate(props.dateString)}</Text>
                 <View
                     style={{
                         borderBottomColor: 'black',
                         borderBottomWidth: 3,
-                        marginHorizontal:5,
-                        marginRight:85,
+                        marginHorizontal: 5,
+                        marginRight: 85,
                     }}
                 />
-                <Text >{props.descriptionEvent}</Text>
-                <Text> Brasserie mise à l'honneur : {brewery.breweryName}</Text>
+                <View style={{margin:2,marginLeft:12}}>
+                    <Text >{props.descriptionEvent}</Text>
+                  <Text style={{fontSize:12}}>Brasserie mise à l'honneur : {brewery.breweryName}</Text>
+                </View>
 
             </View>
         )
@@ -51,41 +53,80 @@ const MyEvents = () => {
                 data={listEvent}
                 keyExtractor={(item) => item.idEvent}
                 renderItem={({ item }) =>
-                    <EventInfo descriptionEvent={item.eventDescript} dateString={item.eventDate} breweryId={item.idBrewery}/>
+                    <EventInfo descriptionEvent={item.eventDescript} dateString={item.eventDate} breweryId={item.idBrewery} />
                 }
 
             />
-             <FlatList
-
-data={listEvent}
-keyExtractor={(item) => item.idEvent}
-renderItem={({ item }) =>
-    <EventInfo descriptionEvent={item.eventDescript} dateString={item.eventDate} breweryId={item.idBrewery}/>
-}
-
-/>
-<FlatList
-
-data={listEvent}
-keyExtractor={(item) => item.idEvent}
-renderItem={({ item }) =>
-    <EventInfo descriptionEvent={item.eventDescript} dateString={item.eventDate} breweryId={item.idBrewery}/>
-}
-
-/>
-<FlatList
-
-data={listEvent}
-keyExtractor={(item) => item.idEvent}
-renderItem={({ item }) =>
-    <EventInfo descriptionEvent={item.eventDescript} dateString={item.eventDate} breweryId={item.idBrewery}/>
-}
-
-/>
-
         </ScrollView>
     )
 }
-
+function stringToDate(string) {
+    let date=new Date(string).toString();
+    let day = date.slice(0, 3);
+    let month= date.slice(4,7)
+    let numeroDay= date.slice(8,11);
+    switch (day) {
+        case 'Mon':
+            day = 'Lundi';
+            break;
+        case 'Tue':
+            day = 'Mardi';
+            break;
+        case 'Wed':
+            day = 'Mercredi';
+            break;
+        case 'Thu':
+            day = 'Jeudi';
+            break;
+        case 'Fri':
+            day = 'Vendredi';
+            break;
+        case 'Sat':
+            day = 'Samedi';
+            break;
+        case 'Sun':
+            day = 'Dimanche';
+            break;
+    }
+    switch (month) {
+        case 'Jan':
+            month = 'Janvier';
+            break;
+        case 'Feb':
+            month = 'Février';
+            break;
+        case 'Mar':
+            month = 'Mars';
+            break;
+        case 'Apr':
+            month = 'Avril';
+            break;
+        case 'May':
+            month = 'Mai';
+            break;
+        case 'Jun':
+            month = 'Juin';
+            break;
+        case 'Jul':
+            month = 'Juillet';
+            break;
+		case 'Aug':
+            month = 'Août';
+            break;
+		case 'Sep':
+            month = 'Septembre';
+            break;
+		case 'Oct':
+            month = 'Octobre';
+            break;
+		case 'Nov':
+            month = 'Novembre';
+            break;
+		case 'Dec':
+            month = 'Décembre';
+            break;
+    }
+    return day+' '+numeroDay+' '+month;
+}
 
 export default MyEvents;
